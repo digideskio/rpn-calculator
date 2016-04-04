cd3603410a0278513eeab116afc31a86a7b629fe
Zachary T Jones <zt.jones88@gmail.com>
2016-04-04 17:11:22 -0400

    Add usage message when script is initially run

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -4,11 +4,22 @@ class RpnCalculator
   attr_accessor :prompt
   attr_reader   :interpreter
 
+  USAGE = <<-END
+************************************************
+*              RPN Calculator                  *
+*                 v0.0.1                       *
+************************************************
+
+q or Ctrl-D to quit.
+  END
+
+  USAGE.freeze
   def initialize
     @prompt = 'rpn-calculator> '
   end
 
   def start_repl
+    puts USAGE
     @interpreter = Interpreter.new
     print @prompt
     loop do
